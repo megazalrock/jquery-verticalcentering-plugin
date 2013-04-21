e8988c5b2675c299b33a2bc4d329ecc3f0369244
otto <otto@mgzl.jp>
2013-04-21 21:42:27 +0900

    引数の引渡し方法を変更

--- jquery-verticalcentering-plugin.js
+++ jquery-verticalcentering-plugin.js
@@ -6,18 +6,13 @@
 * License: MIT License*/
 (function(){
 	var fn = {
-		verticalCentering:function(target,_options){
-			if($.isPlainObject(target)){
-				_options = target;
+		verticalCentering:function(target,shift){
+			shift = shift || 0;
+			if(typeof target === 'number'){
+				shift = target;
 				target = undefined;
-			}else{
-				_options = _options || {};
 			}
-			var options = {
-				shift:0
-			};
-			$.extend(true,options,_options);
-			var $self = $(this), $target = ((target === undefined) ? $self.parent() : $(target)), changeSize;
+			var $self = $(this), $target = ((target === undefined) ? $self.parent() : $(target));
 			var i = 0,length = $self.length;
 			$self.each(function(){
 				$self = $(this);
@@ -25,7 +20,7 @@
 				if(targetHeight > selfHeight){
 					$self
 						.css({
-							marginTop:(targetHeight - selfHeight)/2 + options.shift
+							marginTop:(targetHeight - selfHeight)/2 + shift
 						});
 				}
 			});
