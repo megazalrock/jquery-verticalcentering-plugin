8de727321e5aaf7434c865f0a45c4e45a1b94085
otto <otto@mgzl.jp>
2013-04-21 20:11:09 +0900

    mini fix

--- jquery-verticalcentering-plugin.js
+++ jquery-verticalcentering-plugin.js
@@ -1,4 +1,9 @@
 /*global $:true, jQuery:true */
+/*! jQuery Vertical Centering Plugin 2013-4-21
+* Vertion: 1.0
+* Depends: jQuery 1.8.0 - 1.9.1
+* Author: Otto Kamiya (MegazalRock) http://mgzl.jp/
+* License: MIT License*/
 (function(){
 	var fn = {
 		verticalCentering:function(target,_options){
@@ -13,6 +18,7 @@
 			};
 			$.extend(true,options,_options);
 			var $self = $(this), $target = ((target === undefined) ? $self.parent() : $(target)), changeSize;
+			var i = 0,length = $self.length;
 			$self.each(function(){
 				$self = $(this);
 				var selfHeight = $self.outerHeight(),targetHeight = $target.innerHeight();
@@ -21,15 +27,9 @@
 						.css({
 							marginTop:(targetHeight - selfHeight)/2 + options.shift
 						});
-					$target
-						.css({
-							overflow:'hidden'
-						});
 				}
 			});
 		}
 	};
-
 	$.fn.extend(fn);
-
 })(jQuery);
